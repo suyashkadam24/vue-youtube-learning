d779fa4d61b068ed3e67f3175f3be9e292b637aa
suyashkadam24 <132773844+suyashkadam24@users.noreply.github.com>
2026-05-02 09:57:16 +0000

    commit

---
+++ vue-components-demo/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <div>{{ message }}</div>
+        Contact Owner Name: <input v-model="ownername">
+        <div class="row">
+            <div class="col-12" v-for="contact in contacts" :key="contact.name">
+                <Contact 
+                    :name="contact.name" 
+                    :phone="contact.phone" 
+                    :ownername="contact.ownername" 
+                    :email="contact.email" 
+                    :isFavorite="contact.isFavorite"
+                />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup> 
+import { ref, reactive } from 'vue';
+import Contact from './components/Contact.vue';
+const message = 'Hello Vue!';
+const ownername = ref("Suyash Kadam");
+const contacts=reactive([
+    { name: "Suyash", phone: 1234567890, ownername: ownername, isFavorite: true },
+    { name: "Kadam", phone: 12345654545, ownername: ownername, email: "sk@gmail.com", isFavorite: false },
+    {name: "John Doe", phone: 9876543210, ownername: ownername, email: "john@gmail.com", isFavorite: false }
+]);
+</script>
+
+<style>
+
+</style>
---
+++ vue-components-demo/src/components/Contact.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="bg-info rounded  p-4 m-2">
+        <div class="row">
+            <div class="col-12">
+                <h3>Name: {{ name }}</h3>
+                <p>Email: {{ email }}</p>
+                <p>Phone: {{ phone }}</p>
+                <p>Is Favorite: {{ isFavorite }}</p>
+                <p class="float-end small" v-if="ownername!=''">This contact info belongs to {{ ownername }}</p>
+            </div>
+        </div>
+        
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+// const email = ref("");
+const props = defineProps({ 
+    name: { type: String, required: true },
+    phone: Number,
+    ownername: String,
+    email: { type: String,required: false, default: "-n/a-"},
+    isFavorite: { type: Boolean, default: false }
+});
+</script>
